e0dc46f8ab1beb2646bd54efb35e04664702bbd1
trucalice <nguyenthithanhhuong99992006@gmail.com>
2024-12-17 19:30:41 +0700

    Add files via upload

--- bài 6.cpp
+++ bài 6.cpp
@@ -1,16 +1,23 @@
 #include<stdio.h>
 
 int main(){
- int munber;
- printf("%-4s CALCULATOR")
- printf("1.Tong 2 so");
- printf("2.Hieu 2 so");
- printf("3.Tich 2 so");
- printf("4.Thuong 2 so");
- printf("5.Thoat");
- switch (number)
- case (1)
-  
-return 0;
-}
+    int number, ucln_a,ucln_b;
+    for (int i = 1;i <= ucln_a && ucln_b;i++){
+    	if (a % i == 0 && b % i == 0) {
+            ucln = i;
+        }
+	}	
+    return ucln;
+    int a,b;
+    printf("hay nhap so nguyen duong a: \n");
+    scanf("%d%d",&a);
+    printf("hay nhap so nguyen duong a: \n");
+    scanf("%d%d",&b);
+    if (a <= 0 || b <= 0) {
+        printf("nhap hai so nguyên duong!\n");
+        return 1;
+        int ucln = (so1, so2);
+    printf("uoc chung lon nhat cua %d và %d là: %d\n", so1, so2, ucln);
+	return 0;
+	}
 
