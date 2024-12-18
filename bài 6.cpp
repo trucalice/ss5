68523606c2866cff1ece5653bcb85c3c008638c3
trucalice <nguyenthithanhhuong99992006@gmail.com>
2024-12-18 08:16:59 +0700

    Add files via upload

--- bài 6.cpp
+++ bài 6.cpp
@@ -1,23 +1,40 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int number, ucln_a,ucln_b;
-    for (int i = 1;i <= ucln_a && ucln_b;i++){
-    	if (a % i == 0 && b % i == 0) {
-            ucln = i;
-        }
-	}	
-    return ucln;
-    int a,b;
-    printf("hay nhap so nguyen duong a: \n");
-    scanf("%d%d",&a);
-    printf("hay nhap so nguyen duong a: \n");
-    scanf("%d%d",&b);
-    if (a <= 0 || b <= 0) {
-        printf("nhap hai so nguyên duong!\n");
-        return 1;
-        int ucln = (so1, so2);
-    printf("uoc chung lon nhat cua %d và %d là: %d\n", so1, so2, ucln);
+
+int main() {
+    int n=0,a,b;
+	printf("nhap vao 2 so bat ki: \n");
+	scanf("%d",&a);
+	scanf("%d",&b);
+	printf(" CALCULATOR\n");
+	printf("\n");
+	printf("1. tong 2 so\n");
+	printf("2. hieu 2 so\n");
+	printf("3. tich 2 so\n");
+	printf("4. thuong 2 so\n");
+	printf("5. thoat\n");
+	printf("lua chon cua ban: ");
+	scanf("%d",&n);
+while(n<1 || n>5) {
+	printf("moi nhap lai lua chon tu 1 - 5: ");
+	scanf("%d",&n);
+}
+
+switch (n) {
+	case 1: 
+			printf("%d + %d = %d",a+b);
+		break;
+	case 2:
+			printf("%d - %d = %d",a-b);
+		break;
+	case 3:
+			printf("%d * %d = %d",a*b);
+		break;
+	case 4:
+			printf("%d / %d = %.2f",(float)a/b);
+		break;
+	case 5:
+		break;
+}
 	return 0;
-	}
-
+}
